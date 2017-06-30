ba257127e38cd0a658dcb14d585ef94f78fe6290
Fernando Valdez <fvaldez.mail@gmail.com>
2017-06-29 18:32:26 -0600

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,5 +1,5 @@
 <?php  
- $connect = mysqli_connect('localhost', 'id2026509_fvaldez', '1234567', 'id2026509_registration');  
+ $connect = mysqli_connect('localhost', 'usuario_base_datos', 'contraseña', 'nombre_de_base_datos');  
  $sql = "INSERT INTO tbl_sample(categoria, nombre, descripcion, cantidad, estado) VALUES('".$_POST["categoria"]."', '".$_POST["nombre"]."', '".$_POST["descripcion"]."', '".$_POST["cantidad"]."', '".$_POST["estado"]."')";  
  if(mysqli_query($connect, $sql))  
  {  
