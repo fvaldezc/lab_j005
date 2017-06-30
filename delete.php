4512f0d6b44dd058965999cb3be78ae8bd5bc750
Fernando Valdez <fvaldez.mail@gmail.com>
2017-06-29 18:29:53 -0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php  
- $connect = mysqli_connect('localhost', 'id2026509_fvaldez', '1234567', 'id2026509_registration');  
+ $connect = mysqli_connect('localhost', 'usuario_base_datos', 'contraseña', 'nombre_de_base_datos');  
  $sql = "DELETE FROM tbl_sample WHERE id = '".$_POST["id"]."'";  
  if(mysqli_query($connect, $sql))  
  {  
